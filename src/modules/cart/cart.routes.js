3213a9d4686ffc12adf56f304dc16967a659bb2a
Jenisha J <jenisha.j@bounteous.com>
2026-03-04 17:36:10 +0530

    Modified

--- src/modules/cart/cart.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { protect } from "../../middleware/auth.middleware.js";
-import { addToCart, getCart } from "./cart.controller.js";
-const router = Router();
-router.post("/", protect, addToCart);
-router.get("/", protect, getCart);
-export default router;
